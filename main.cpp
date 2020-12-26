b238c68a0ce044f01247d600b98deeb42abdacf5
zhangxing <zhangxingmsn@gmail.com>
2020-12-26 21:49:02 +0800

    13.常量引用

--- main.cpp
+++ main.cpp
@@ -1,37 +1,29 @@
 #include<iostream>
 using namespace std;
 
-struct Person
-{
-    int m_Age;
-};
-
-void allocatMemory(Person ** p) // **p 具体的Person对象  *p 对象的指针   p 指针的指针
-{
-    *p = (Person *)malloc(sizeof(Person));
-
-    (*p)->m_Age = 100;
-}
-
 void test01()
 {
-    Person * p = NULL;
-    allocatMemory(&p);
-    cout <<  "p的年龄： " <<p->m_Age << endl;
+    //int &ref = 10;//引用了不合法的内存，不可以
+    const int &ref = 10; //加入const后 ，编译器处理方式为： int tmp = 10; const int &ref = tmp;
+    //ref = 10;  //不合法
+
+    int * p = (int*)&ref;
+    *p = 1000;
+
+    cout << "ref = " << ref << endl;
 }
 
-//利用指针引用开辟空间
-void allocatMemoryByRef(Person* &p)
+//常量引用使用场景 用来修饰形参
+void showValue(const int &val)
 {
-    p = (Person*)malloc(sizeof(Person));
-    p->m_Age = 1000;
+    //val += 1000; //如果只是想显示内容，而不修改内容，那么就用const修饰这个形参
+    cout << "val = " << val << endl;
 }
 
 void test02()
 {
-    Person * p = NULL;
-    allocatMemoryByRef(p);
-    cout << "p的年龄：" << p->m_Age << endl;
+    int a = 10;
+    showValue(a);
 }
 
 int main()
