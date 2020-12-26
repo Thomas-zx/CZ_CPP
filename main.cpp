6ddba75a373650d779cc458a3539f703daabf645
zhangxing <zhangxingmsn@gmail.com>
2020-12-26 21:42:35 +0800

    11.参数的传递方式

--- main.cpp
+++ main.cpp
@@ -1,66 +1,104 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include<iostream>
 using namespace std;
 
-//1、引用基本语法  Type &别名 = 原名
+//1.值传递
+void mySwap(int a, int b)
+{
+    int tmp = a;
+    a = b;
+    b = tmp;
+
+    cout << "mySwap::a = " << a << endl;
+    cout << "mySwap::b = " << b << endl;
+}
+
 void test01()
 {
     int a = 10;
-    int &b = a;
-
-    b = 20;
+    int b = 20;
+    mySwap(a, b); //值传递
 
     cout << "a = " << a << endl;
     cout << "b = " << b << endl;
 }
 
-//2、引用必须初始化
+//2.地址传递
+void mySwap2(int * a, int * b)
+{
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+
 void test02()
 {
-    //int &a; 必须初始化
     int a = 10;
-    int &b = a; //引用初始化后不可以修改了
-    int c = 20;
+    int b = 20;
+    mySwap2(&a, &b);
 
-    b = c; //赋值！！！
     cout << "a = " << a << endl;
     cout << "b = " << b << endl;
 }
 
-//3、对数组建立引用
+//3.引用传递 类似传地址
+void mySwap3(int &a, int &b)  //&a = a &b = b
+{
+    int tmp = a;
+    a = b;
+    b = tmp;
+}
+
 void test03()
 {
-    int arr[10];
+    int a = 10;
+    int b = 20;
+    mySwap3(a, b);
 
-    for (int i = 0; i < 10;i++)
-    {
-        arr[i] = i;
-    }
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+}
 
-    //给数组起别名
-    int(&pArr)[10] = arr;
-    for (int i = 0; i < 10;i++)
-    {
-        cout << pArr[i] << " ";
-    }
-    cout << endl;
+//引用的注意事项
+//1、引用必须引一块合法的内存空间
+//2、不要返回局部变量的引用
+int& doWork()
+{
+    int a = 10;
+    return a;
+}
+void test04()
+{
+    //int &a = 10; // 引用必须引一块合法的内存空间
+    int &ret = doWork();
+    cout << "ret = " << ret << endl; //第一次10是编译器做了优化
+    cout << "ret = " << ret << endl;
+    cout << "ret = " << ret << endl;
+    cout << "ret = " << ret << endl;
+    cout << "ret = " << ret << endl;
+}
 
-    //第二种方式 起别名
-    typedef int(ARRAYREF)[10]; //一个具有10个元素的int类型的数组
-    ARRAYREF & pArr2 = arr;
+int& doWork2()
+{
+    static int a = 10;
+    cout << "a = " << a << endl;
+    return a;
+}
+void test05()
+{
+    int &ret = doWork2();
 
-    for (int i = 0; i < 10; i++)
-    {
-        cout << pArr2[i] << " ";
-    }
-    cout << endl;
+    //如果函数的返回值是引用，那么这个函数调用可以作为左值
+    doWork2() = 1000; //相当于写了 a = 1000;
+    cout << "ret = " << ret << endl;
 }
 
 int main()
 {
     //test01();
     //test02();
-    test03();
+    //test03();
+    //test04();
+    test05();
 
     system("pause");
     return EXIT_SUCCESS;
