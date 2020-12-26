1bff93f475207310a0d9895293350cb84dbe0dae
zhangxing <zhangxingmsn@gmail.com>
2020-12-26 21:45:49 +0800

    12.指针的引用

--- main.cpp
+++ main.cpp
@@ -1,104 +1,43 @@
 #include<iostream>
 using namespace std;
 
-//1.值传递
-void mySwap(int a, int b)
+struct Person
 {
-    int tmp = a;
-    a = b;
-    b = tmp;
+    int m_Age;
+};
 
-    cout << "mySwap::a = " << a << endl;
-    cout << "mySwap::b = " << b << endl;
+void allocatMemory(Person ** p) // **p 具体的Person对象  *p 对象的指针   p 指针的指针
+{
+    *p = (Person *)malloc(sizeof(Person));
+
+    (*p)->m_Age = 100;
 }
 
 void test01()
 {
-    int a = 10;
-    int b = 20;
-    mySwap(a, b); //值传递
-
-    cout << "a = " << a << endl;
-    cout << "b = " << b << endl;
+    Person * p = NULL;
+    allocatMemory(&p);
+    cout <<  "p的年龄： " <<p->m_Age << endl;
 }
 
-//2.地址传递
-void mySwap2(int * a, int * b)
+//利用指针引用开辟空间
+void allocatMemoryByRef(Person* &p)
 {
-    int tmp = *a;
-    *a = *b;
-    *b = tmp;
+    p = (Person*)malloc(sizeof(Person));
+    p->m_Age = 1000;
 }
 
 void test02()
 {
-    int a = 10;
-    int b = 20;
-    mySwap2(&a, &b);
-
-    cout << "a = " << a << endl;
-    cout << "b = " << b << endl;
-}
-
-//3.引用传递 类似传地址
-void mySwap3(int &a, int &b)  //&a = a &b = b
-{
-    int tmp = a;
-    a = b;
-    b = tmp;
-}
-
-void test03()
-{
-    int a = 10;
-    int b = 20;
-    mySwap3(a, b);
-
-    cout << "a = " << a << endl;
-    cout << "b = " << b << endl;
-}
-
-//引用的注意事项
-//1、引用必须引一块合法的内存空间
-//2、不要返回局部变量的引用
-int& doWork()
-{
-    int a = 10;
-    return a;
-}
-void test04()
-{
-    //int &a = 10; // 引用必须引一块合法的内存空间
-    int &ret = doWork();
-    cout << "ret = " << ret << endl; //第一次10是编译器做了优化
-    cout << "ret = " << ret << endl;
-    cout << "ret = " << ret << endl;
-    cout << "ret = " << ret << endl;
-    cout << "ret = " << ret << endl;
-}
-
-int& doWork2()
-{
-    static int a = 10;
-    cout << "a = " << a << endl;
-    return a;
-}
-void test05()
-{
-    int &ret = doWork2();
-
-    //如果函数的返回值是引用，那么这个函数调用可以作为左值
-    doWork2() = 1000; //相当于写了 a = 1000;
-    cout << "ret = " << ret << endl;
+    Person * p = NULL;
+    allocatMemoryByRef(p);
+    cout << "p的年龄：" << p->m_Age << endl;
 }
 
 int main()
 {
     //test01();
-    //test02();
-    //test03();
-    //test04();
-    test05();
+    test02();
 
     system("pause");
     return EXIT_SUCCESS;
