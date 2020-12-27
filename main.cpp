1e69a63a6e098327ae1851311db6662a47821550
张兴 <zhangxingmsn@gmail.com>
2020-12-27 11:34:49 +0800

    19.C++语言下的封装

--- main.cpp
+++ main.cpp
@@ -1,54 +1,77 @@
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
+#include<iostream>
+using namespace std;
 
 struct Person
 {
     char mName[64];
     int mAge;
+
+    void PersonEat()
+    {
+        cout <<  mName <<"吃人饭" << endl;
+    }
 };
-
-void PersonEat(struct Person *p)
-{
-    printf("%s 在吃人饭 \n",p->mName);
-}
-
-void test01()
-{
-    struct Person p1;
-    strcpy(p1.mName, "德玛西亚");
-
-    PersonEat(&p1);
-}
 
 struct Dog
 {
     char mName[64];
     int mAge;
+
+    void DogEat()
+    {
+        cout << mName << "吃狗粮" << endl;
+    }
 };
 
-void DogEat(struct Dog * dog)
+//C++中的封装 严格类型转换检测，让属性和行为 绑定到一起
+// 1 属性和行为作为一个整体来表示生活中的事物
+// 2 控制权限 public 公有权限 protected 保护权限 private 私有权限
+void test01()
 {
-    printf("%s 在吃狗粮 \n", dog->mName);
+    Person p1;
+    strcpy(p1.mName, "老王");
+    p1.PersonEat();
+    //p1.DogEat();
 }
 
+//struct 和class是一个意思，唯一的不同  默认权限 ，struct是public，但是class默认权限是private
+class Animal
+{
+private:
+    int mAge;
+    //如果我不声明权限，默认的权限是 private
+    void eat(){ mAge = 100; };
+
+public:
+    int mHeight;
+
+protected: //保护权限 类内部可以访问 ，(当前类的子类可以访问) , 类外部不可以访问
+    int mWeight;
+
+    void setWeight(){ mWeight = 100; };
+};
+
+//所谓私有权限 就是私有成员(属性、函数) 在类内部可以访问，类外部不可以方法
+//公共权限 ，在类内部和类外部都可以访问
 void test02()
 {
-    struct Dog d;
-    strcpy(d.mName, "旺财");
-    DogEat(&d);
+    Animal an;
+    //an.eat();
+    //an.mAge; //私有不可以访问到
 
-    struct Person p1;
-    strcpy(p1.mName, "老王");
-    //DogEat(&p1);
-
+    an.mHeight = 100; //公有权限在类外部可以访问到
+    //an.mWeight = 100; //保护权限 在类外不可访问到
 }
-//C语言封装 属性和行为分开处理了 ,类型检测不够
+
+// public  类内 类外 都可以访问
+// protected 类内可以访问 类外 不可以访问 （子类可以访问）
+// private   类内可以访问 类外 不可以访问 （子类不可以访问）
 
 int main()
 {
     //test01();
     test02();
+
     system("pause");
     return EXIT_SUCCESS;
 }
