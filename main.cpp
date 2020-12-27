b23d6413d70575599dbfd0dab55a1d17f74333ef
张兴 <zhangxingmsn@gmail.com>
2020-12-27 13:33:46 +0800

    25.深拷贝和浅拷贝

--- main.cpp
+++ main.cpp
@@ -1,71 +1,54 @@
 #include<iostream>
 using namespace std;
 
-class MyClass
+class Person
 {
 public:
-    //MyClass()
-    //{
-    //	cout << "默认构造函数" << endl;
-    //}
-
-    MyClass(int a)
+    Person()
+    {}
+    //初始化属性
+    Person(char * name,int age)
     {
-        cout << "有参构造函数" << endl;
+        m_Name = (char*)malloc(strlen(name) + 1);
+        strcpy(m_Name, name);
+
+        m_age = age;
+    }
+    //拷贝构造 系统会提供默认拷贝构造，而且是简单的值拷贝
+    //自己提供拷贝构造，原因简单的浅拷贝会释放堆区空间两次，导致挂掉
+    //深拷贝
+    Person(const Person&p)
+    {
+        m_age = p.m_age;
+        m_Name = (char*)malloc(strlen(p.m_Name) + 1);
+        strcpy(m_Name, p.m_Name);
     }
 
-    //MyClass(const MyClass& m)
-    //{
-    //}
-    int m_A;
+    ~Person()
+    {
+        cout << "析构函数调用" << endl;
+        if (m_Name != NULL)
+        {
+            free(m_Name);
+            m_Name = NULL;
+        }
+    }
 
-
+    //姓名
+    char * m_Name;
+    //年龄
+    int m_age;
 };
-
-//系统默认给一个类提供 3个函数  默认构造 、 拷贝构造 、 析构函数
-
-//1 当我们提供了有参构造函数，那么系统就不会在给我们提供默认构造函数了
-//但是 系统还会提供默认拷贝构造函数 , 进行简单的值拷贝
 
 void test01()
 {
-    MyClass c1(1);
-    c1.m_A = 10;
-    MyClass c2(c1);
-    cout << c2.m_A << endl;
+    Person p1("敌法",10);
+    Person p2(p1); //调用拷贝构造
 }
-
-//2 当我们提供了 拷贝构造，那么系统就不会提供其他构造了
-class MyClass2
-{
-public:
-    MyClass2()
-    {
-        cout << "默认构造函数" << endl;
-    }
-
-    //MyClass2(int a)
-    //{
-    //	cout << "有参构造函数" << endl;
-    //}
-
-    MyClass2(const MyClass& m)
-    {
-    }
-    int m_A;
-
-};
-
-void test02()
-{
-    MyClass2 c1;
-}
-
 
 int main()
 {
-    //test01();
-    test02();
+    test01();
 
     system("pause");
     return EXIT_SUCCESS;
