a426749c6c8630fdb5d29468c6ddf3bfaafc3038
张兴 <zhangxingmsn@gmail.com>
2020-12-27 14:20:06 +0800

    36.const修饰成员函数

--- main.cpp
+++ main.cpp
@@ -1,31 +1,47 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
 class Person
 {
-public:
-    void show()
+public :
+    Person()
     {
-        cout << "Person show" << endl;
+        //构造中修改属性
+        //this 永远执行本体
+        this->m_A = 0;
+        this->m_B = 0;
     }
 
-    void showAge()
+    void showInfo() const //常函数 不允许修改指针指向的值
     {
-        if (this == NULL)
-        {
-            return;
-        }
-        cout << this->m_Age << endl; // NULL -> m_Age
+        //this->m_A = 1000;
+        this->m_B = 1000;
+        // const Person * const this
+        cout << "m_A = " << this->m_A << endl;
+        cout << "m_B = " << this->m_B << endl;
     }
 
-    int m_Age; //
+    void show2() const
+    {
+        //m_A = 100;
+    }
+
+    int m_A;
+    mutable int m_B; //就算是常函数 我还是执意要修改
 };
 
 void test01()
 {
-    Person * p = NULL;
-    p->show();
-    p->showAge();
+
+    Person p1;
+    p1.showInfo();
+
+    //常对象 不允许修改属性
+    const  Person p2;
+    cout << p2.m_A << endl;
+    p2.show2();
+    //常对象 不可以调用普通成员函数
+    //常对象 可以调用常函数
 }
 
 int main()
