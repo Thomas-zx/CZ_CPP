05fac02e8aa334f527d822042f0212e963e00db8
张兴 <zhangxingmsn@gmail.com>
2020-12-27 11:56:04 +0800

    21.面向对象设计案例-立方体案例

--- main.cpp
+++ main.cpp
@@ -1,63 +1,130 @@
-#include <iostream>
-#include <string>
+#include<iostream>
 using namespace std;
 
-class Person
+/*
+设计立方体类(Cube)，
+求出立方体的面积( 2*a*b + 2*a*c + 2*b*c )和体积( a * b * c)，
+分别用全局函数和成员函数判断两个立方体是否相等
+*/
+
+class Cube
 {
 public:
-    //设置年龄
-    void setAge(int age)
+    //设置长
+    void setL(int l)
     {
-        if (age < 0 || age > 100)
-        {
-            cout << "你这个老妖精" << endl;
-            return;
-        }
-        m_Age = age;
-    }
-    //获取年龄 读权限
-    int getAge()
-    {
-        return m_Age;
+        m_L = l;
     }
 
-    //读姓名
-    string getName()
+    //获取长
+    int getL() const
     {
-        return m_Name;
+        return m_L;
     }
-    //写姓名
-    void setName(string name)
+    //设置宽
+    void setW(int w)
     {
-        m_Name = name;
-    }
-
-    //只写的情人
-    void setLover(string lover)
-    {
-        m_lover = lover;
+        m_W = w;
     }
 
-private: //类外不可访问，类内可以访问
-    int m_Age  = 0; //年龄 读写
-    string m_Name; //公有权限  读写
-    string m_lover; //情人  只写
+    //获取宽
+    int getW()
+    {
+        return m_W;
+    }
 
+    //设置高
+    void setH(int h)
+    {
+        m_H = h;
+    }
+
+    //获取高
+    int getH()
+    {
+        return m_H;
+    }
+
+    //求立方体的面积
+    void getCubeS()
+    {
+        int s = 2 * m_L*m_W + 2 * m_W*m_H + 2 * m_L*m_H;
+        cout << "立方体的面积为： " << s << endl;;
+    }
+
+    //求立方体体积
+    void getCubeV()
+    {
+        int v = m_L * m_W * m_H;
+        cout << "立方体的体积为： " << v << endl;
+    }
+
+    //通过成员函数判断是否相等
+    bool compareCubeByClass( Cube & cub)
+    {
+        bool ret = m_L == cub.getL() && m_W == cub.getW() && m_H == cub.getH();
+        return ret;
+    }
+
+private:
+    int m_L; //长
+    int m_W; //宽
+    int m_H; //高
 };
+
+//为什么const刚刚不能添加？ 没法保证成员函数里是否修改了成员属性
+void func(const Cube & cub)
+{
+    cub.getL();
+}
+
+//全局函数判断 两个立方体是否相等
+bool compareCube( Cube &cub1,  Cube & cub2)
+{
+    if (cub1.getL() == cub2.getL()  && cub1.getW() == cub2.getW() && cub1.getH() == cub2.getH())
+    {
+        return true;
+    }
+    return false;
+}
 
 void test01()
 {
-    Person p1;
+    Cube c1;
+    c1.setL(10);
+    c1.setW(10);
+    c1.setH(10);
 
-    p1.setName("老王");
-    cout << "p1的姓名：" << p1.getName() << endl;
+    c1.getCubeS(); // 600
+    c1.getCubeV(); // 1000
 
-    //年龄
-    p1.setAge(120);
-    cout << "p1的年龄：" << p1.getAge() << endl;
+    //通过全局函数判断两个立方体是否相等
 
-    //情人 只能设置 外部我不告诉你
-    p1.setLover("仓井");
+    Cube c2;
+    c2.setL(10);
+    c2.setW(10);
+    c2.setH(10);
+
+    bool ret = compareCube(c1, c2);
+    if ( ret)
+    {
+        cout << "c1 和 c2 是相等的！" << endl;
+    }
+    else
+    {
+        cout << "c1 和 c2是不等的！" << endl;
+    }
+
+    //通过成员函数判断立方体是否相等
+    bool ret2 = c1.compareCubeByClass(c2);
+    if (ret2)
+    {
+        cout << "通过成员：：c1 和 c2 是相等的！" << endl;
+    }
+    else
+    {
+        cout << "通过成员：：c1 和 c2是不等的！" << endl;
+    }
 }
 
 int main()
