c0cfd012bb13a17c550144118cc42565650d3358
张兴 <zhangxingmsn@gmail.com>
2020-12-29 16:03:14 +0800

    49.继承的引出

--- main.cpp
+++ main.cpp
@@ -1,50 +1,144 @@
-#include <iostream>
-#include "48_MyString.h"
+#include<iostream>
 using namespace std;
 
-//测试 MyString
-void test01()
+//class News
+//{
+//public:
+//	void header()
+//	{
+//		cout << "公共头部" << endl;
+//	}
+//	void footer()
+//	{
+//		cout << "公共底部" << endl;
+//	}
+//	void left()
+//	{
+//		cout << "左侧列表" << endl;
+//	}
+//
+//	void content()
+//	{
+//		cout << "新闻播放" << endl;
+//	}
+//
+//};
+//
+//class YULE
+//{
+//public:
+//	void header()
+//	{
+//		cout << "公共头部" << endl;
+//	}
+//	void footer()
+//	{
+//		cout << "公共底部" << endl;
+//	}
+//	void left()
+//	{
+//		cout << "左侧列表" << endl;
+//	}
+//
+//	void content()
+//	{
+//		cout << "白百合。。。" << endl;
+//	}
+//};
+//
+//void test01()
+//{
+//	News news;
+//	news.header();
+//	news.footer();
+//	news.left();
+//	news.content();
+//
+//	//娱乐页
+//	YULE yl;
+//	yl.header();
+//	yl.footer();
+//	yl.left();
+//	yl.content();
+//
+//}
+
+//继承写法
+//抽象一个 基类的网页  重复的代码都写到这个网页上
+class BasePage
 {
-    MyString str = "abc";
-
-    cout << str << endl;
-
-    cout << "请输入str新的内容：" << endl;
-    cin >> str;
-    cout << "新内容为：" << str << endl;
-
-    MyString str2(str);
-    MyString str3 = "aaaaaa";
-
-    str3 = str2;
-    str3 = "aaaa";
-    cout << "str3 = " << str3 << endl;
-
-    str3[0] = 'w';
-    cout << "str3 第一个位置为 = " << str3[0] << endl;
-
-    MyString str4 = "";
-    str4 = str2 + str3; //字符串拼接
-
-    cout << "str4 为 " << str4 << endl;
-
-    if (str3 == str4)
+public:
+    void header()
     {
-        cout << "str3 与 str4相等" << endl;
+        cout << "公共头部" << endl;
     }
-    else
+    void footer()
     {
-        cout << "str3 与 str4不相等" << endl;
+        cout << "公共底部" << endl;
     }
+    void left()
+    {
+        cout << "左侧列表" << endl;
+    }
+};
 
-    /*int a = 10;
-    cin >> a;
-    cout << "a = " << a << endl;*/
+class News :public BasePage //继承  News类 继承于 BasePage类
+{
+public:
+    void content()
+    {
+        cout << "新闻播放" << endl;
+    }
+};
+
+class YULE :public BasePage
+{
+public:
+    void content()
+    {
+        cout << "白百合。。。" << endl;
+    }
+};
+
+class Game :public BasePage
+{
+public:
+    void content()
+    {
+        cout << "KPL直播" << endl;
+    }
+};
+
+void test02()
+{
+    cout << " 新闻网页内容： " << endl;
+    News news;
+    news.header();
+    news.footer();
+    news.left();
+    news.content();
+
+    cout << " 娱乐网页内容： " << endl;
+    YULE yl;
+    yl.header();
+    yl.footer();
+    yl.left();
+    yl.content();
+
+    cout << " 游戏网页内容： " << endl;
+    Game game;
+    game.header();
+    game.footer();
+    game.left();
+    game.content();
 }
 
+//继承 减少代码重复内容
+//BasePage  基类 (父类)   News 派生类 （子类）
 int main()
 {
-    test01();
+    //test01();
+    test02();
 
     system("pause");
     return EXIT_SUCCESS;
