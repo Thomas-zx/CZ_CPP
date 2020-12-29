514bd828f94335d54224b70fd1efe2a472c5cc3c
张兴 <zhangxingmsn@gmail.com>
2020-12-29 16:34:16 +0800

    54.继承中静态成员的处理

--- main.cpp
+++ main.cpp
@@ -4,58 +4,44 @@ using namespace std;
 class Base
 {
 public:
-    Base()
+    static void func()
     {
-        m_A = 100;
+        cout << "base fun()" << endl;
     }
 
-    void fun()
+    static void func(int a)
     {
-        cout << "Base func调用" << endl;
+        cout << "base fun(int)" << endl;
     }
 
-    void fun(int a)
-    {
-        cout << "Base func (int a)调用" << endl;
-    }
-
-    int m_A;
+    static int m_A;
 };
+int Base::m_A = 10;
 
 class Son :public Base
 {
 public:
-    Son()
+    static void func()
     {
-        m_A = 200;
+        cout << "son fun()" << endl;
     }
 
-    void fun()
-    {
-        cout << "Son func调用" << endl;
-    }
-
-    int m_A;
+    static int m_A;
 };
+int Son::m_A = 20;
 
+//静态成员属性 子类可以继承下来
 void test01()
 {
-    Son s1;
-    cout << s1.m_A << endl;
+    cout << Son::Base::m_A << endl;
+    //访问父类的m_A
+    cout << Base::m_A << endl;
 
-    //想调用 父类中 的m_A
-    cout << s1.Base::m_A << endl;
-
-    s1.fun();
-
-    //调用父类的func
-    s1.Base::fun(10);
+    Son::func();
+    //访问 父类中同名的函数
+    Son::Base::func(10);
 
 }
-
-//如果子类和父类拥有同名的函数 属性 ，子类会覆盖父类的成员吗？ 不会
-//如果子类与父类的成员函数名称相同，子类会把父类的所有的同名版本都隐藏掉
-//想调用父类的方法，必须加作用域
 
 int main()
 {
