84a825ff227fff0b89816ddcb1138566314d8e2f
张兴 <zhangxingmsn@gmail.com>
2020-12-31 15:40:55 +0800

    64.函数模板与普通函数的区别以及调用规则

--- main.cpp
+++ main.cpp
@@ -1,64 +1,76 @@
 #include <iostream>
 using namespace std;
 
-//对char和int数组进行排序 排序规则 从大到小 利用选择排序
+//1.普通函数与函数模板区别
 template <class T>
-void mySwap(T &a, T &b)
+T myPlus(T a, T b)
 {
-    T tmp = a;
-    a = b;
-    b = tmp;
+    return a + b;
 }
 
-template <class T>
-void mySort(T arr[], int len)
+int myPlus2(int a, int b)
 {
-    for (int i = 0; i < len; i++)
-    {
-        int max = i;
-        for (int j = i + 1; j < len;j++)
-        {
-            if (arr[max] < arr[j])
-            {
-                //交换 下标
-                max = j;
-            }
-        }
-        if (max != i)
-        {
-            //交换数据
-            mySwap(arr[max], arr[i]);
-        }
-    }
-}
-
-//输出数组元素的模板
-template <class T>
-void printArray(T arr[], int len)
-{
-    for (int i = 0; i < len;i++)
-    {
-        cout << arr[i] << " ";
-    }
-    cout << endl;
+    return a + b;
 }
 
 void test01()
 {
-    char charArr[] = "hello，world";
-    int num = sizeof(charArr) / sizeof(char);
-    mySort(charArr, num);
-    printArray(charArr, num);
+    int a = 10;
+    int b = 20;
+    char c = 'c';
+    //myPlus(a, c); //类型推导不出来,函数模板不可以进行隐式类型转换
+    cout << myPlus2(a, c) <<endl; // 10 + 99  普通函数 可以进行隐式类型转换
+}
 
-    int intArr[] = { 1, 4, 100, 34, 55 };
-    int num2 = sizeof(intArr) / sizeof (int);
-    mySort(intArr, num2);
-    printArray(intArr, num2);
+//2.普通函数和函数模板的调用规则
+template <class T>
+void myPrint(T a, T b)
+{
+    cout << "模板调用的myPrint" << endl;
+}
+
+template<class T>
+void myPrint(T a, T b ,T c)
+{
+    cout << "模板调用的myPrint(a,b,c)" << endl;
+}
+
+void myPrint(int a, int  b)
+{
+    cout << "普通函数调用 myPrint" << endl;
+}
+
+//通过模板生成的函数 叫模板函数
+void myPrint(int a, int b, int c)
+{
+    cout << "普通函数调用的myPrint(a,b,c)" << endl;
+}
+
+void test02()
+{
+    int a = 10;
+    int b = 20;
+
+    //1 、如果出现重载 优先使用普通函数调用,如果没有实现，出现错误
+    myPrint(a, b);
+
+    //2、 如果想强制调用模板 ，那么可以使用空参数列表
+    myPrint<>(a, b);
+
+    //3、 函数模板可以发生重载
+    int c = 30;
+    myPrint(a, b, c);
+
+    //4、 如果函数模板可以产生更好的匹配，那么优先调用函数模板
+    char c1 = 'c';
+    char d = 'd';
+    myPrint(c1, d);
 }
 
 int main()
 {
-    test01();
+    //test01();
+    test02();
 
     system("pause");
     return EXIT_SUCCESS;
