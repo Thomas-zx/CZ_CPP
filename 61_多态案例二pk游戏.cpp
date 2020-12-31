6c0a9a739b923119da8f87023e7008247079ebcc
张兴 <zhangxingmsn@gmail.com>
2020-12-31 10:53:06 +0800

    62.函数模板的基本用法

---
+++ 61_多态案例二pk游戏.cpp
@@ -0,0 +1,97 @@
+//
+// Created by 张兴 on 2020-12-30.
+//
+
+#include <iostream>
+#include <ctime>
+#include "61_Weapon.h"
+#include "61_Knife.h"
+#include "61_DragonSword.h"
+#include "61_Hero.h"
+#include "61_Monster.h"
+
+using namespace std;
+
+void play()
+{
+    //创建怪物
+    Monster * monster = new Monster;
+    //创建英雄
+    Hero * hero = new Hero;
+
+    //创建武器
+    Weapon * kinfe = new Knife;
+    Weapon * dragon = new DragonSword;
+
+    //让用户选择 武器
+    cout << "请选择武器：" << endl;
+    cout << "1. 赤手空拳" << endl;
+    cout << "2. 小刀" << endl;
+    cout << "3. 屠龙刀" << endl;
+
+    int oper;
+    cin >> oper;
+    switch (oper)
+    {
+        case 1:
+            cout << "你真牛X，你还是太年轻了！" << endl;
+            break;
+        case 2:
+            hero->EquipWeapon(kinfe);
+            break;
+        case 3:
+            hero->EquipWeapon(dragon);
+            break;
+    }
+
+    getchar(); //输入缓冲区里有个回车 ，多获取一次值
+
+    int round = 1;
+
+    while (true)
+    {
+        getchar();
+        system("cls");
+
+        cout << "----- 当前第 " << round << " 回合开始 ------" << endl;
+        if (hero->m_Hp <= 0)
+        {
+            cout << "英雄" << hero->m_Name << "已挂 ，游戏结束" << endl;
+            break;
+        }
+        hero->Attack(monster);
+
+        if (monster->m_Hp <= 0)
+        {
+            cout << "怪物" << monster->m_Name << "已挂，顺利通关" << endl;
+            break;
+        }
+        monster->Attack(hero);
+
+
+        if (hero->m_Hp <= 0)
+        {
+            cout << "英雄" << hero->m_Name << "已挂 ，游戏结束" << endl;
+            break;
+        }
+
+        cout << "英雄" << hero->m_Name << "剩余血量：" << hero->m_Hp << endl;
+        cout << "怪物" << monster->m_Name << "剩余血量：" << monster->m_Hp << endl;
+
+        round++;
+    }
+
+    delete monster;
+    delete hero;
+    delete kinfe;
+    delete dragon;
+}
+
+int main()
+{
+    srand((unsigned int)time(NULL));
+    play();
+
+    system("pause");
+    return EXIT_SUCCESS;
+}
--- main.cpp
+++ main.cpp
@@ -1,92 +1,69 @@
 #include <iostream>
-#include <ctime>
-#include "61_Weapon.h"
-#include "61_Knife.h"
-#include "61_DragonSword.h"
-#include "61_Hero.h"
-#include "61_Monster.h"
-
 using namespace std;
 
-void play()
+//交换int类型两个数字
+void mySwapInt(int & a, int & b)
 {
-    //创建怪物
-    Monster * monster = new Monster;
-    //创建英雄
-    Hero * hero = new Hero;
+    int tmp = a;
+    a = b;
+    b = tmp;
+}
 
-    //创建武器
-    Weapon * kinfe = new Knife;
-    Weapon * dragon = new DragonSword;
+//交换double数据
+void mySwapDouble(double &a, double &b)
+{
+    double tmp = a;
+    a = b;
+    b = tmp;
+}
 
-    //让用户选择 武器
-    cout << "请选择武器：" << endl;
-    cout << "1. 赤手空拳" << endl;
-    cout << "2. 小刀" << endl;
-    cout << "3. 屠龙刀" << endl;
+//类型，逻辑又非常相似
+//类型参数化  泛型编程 -- 模板技术
+template <class T> // 告诉编译器 下面如果出现T不要报错，T是一个通用的类型
+void mySwap(T &a, T &b)
+{
+    T tmp = a;
+    a = b;
+    b = tmp;
+}
 
-    int oper;
-    cin >> oper;
-    switch (oper)
-    {
-        case 1:
-            cout << "你真牛X，你还是太年轻了！" << endl;
-            break;
-        case 2:
-            hero->EquipWeapon(kinfe);
-            break;
-        case 3:
-            hero->EquipWeapon(dragon);
-            break;
-    }
+// template<typename T>  等价于 template<class T>
+template <typename T>
+void mySwap2(){}
 
-    getchar(); //输入缓冲区里有个回车 ，多获取一次值
+void test01()
+{
+    int a = 10;
+    int b = 20;
+    char c1 = 'c';
 
-    int round = 1;
+    mySwapInt(a, b);
+    cout << "1.a = " << a  << ",b = " << b << endl;
 
-    while (true)
-    {
-        getchar();
-        system("cls");
+    //1.自动类型推导,必须有参数类型才可以推导
+    //mySwap(a, c1); 推导不出来T，所以不能运行
+    mySwap(a, b);
+    cout << "2.a = " << a  << ",b = " << b << endl;
 
-        cout << "----- 当前第 " << round << " 回合开始 ------" << endl;
-        if (hero->m_Hp <= 0)
-        {
-            cout << "英雄" << hero->m_Name << "已挂 ，游戏结束" << endl;
-            break;
-        }
-        hero->Attack(monster);
+    //2.显示指定类型
+    mySwap<int>(a, b);
+    cout << "3.a = " << a  << ",b = " << b << endl;
+    
+    //mySwap2();  //模板必须要指定出T才可以使用
+    mySwap2<double>();
 
-        if (monster->m_Hp <= 0)
-        {
-            cout << "怪物" << monster->m_Name << "已挂，顺利通关" << endl;
-            break;
-        }
-        monster->Attack(hero);
+    double c = 3.14;
+    double d = 1.1;
+    mySwapDouble(c, d);
+    cout << "4.c = " << c  << ",d = " << d << endl;
 
-
-        if (hero->m_Hp <= 0)
-        {
-            cout << "英雄" << hero->m_Name << "已挂 ，游戏结束" << endl;
-            break;
-        }
-
-        cout << "英雄" << hero->m_Name << "剩余血量：" << hero->m_Hp << endl;
-        cout << "怪物" << monster->m_Name << "剩余血量：" << monster->m_Hp << endl;
-
-        round++;
-    }
-
-    delete monster;
-    delete hero;
-    delete kinfe;
-    delete dragon;
+    mySwap(c, d);
+    cout << "5.c = " << c  << ",d = " << d << endl;
 }
 
 int main()
 {
-    srand((unsigned int)time(NULL));
-    play();
+    test01();
 
     system("pause");
     return EXIT_SUCCESS;
