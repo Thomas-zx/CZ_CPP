5cbe0a3841197b9b3913913a3951c886d22531c6
张兴 <zhangxingmsn@gmail.com>
2021-01-04 17:00:00 +0800

    72.类模板碰到友元函数-类外实现

--- main.cpp
+++ main.cpp
@@ -2,14 +2,20 @@
 #include <string>
 using namespace std;
 
-template<class T1 ,class T2>
+//让编译器提前看到printPerson声明
+
+//让编译器看到Person类声明
+template<class T1, class T2> class Person;
+template<class T1, class T2> void printPerson(Person<T1, T2> & p);
+
+template<class T1, class T2>
 class Person
 {
-    //友元函数类内实现
-    friend void printPerson( Person<T1 ,T2> & p )
-    {
+    //友元函数类内实现  利用空参数列表 告诉编译器 模板函数的声明
+    friend void printPerson<>(Person<T1, T2> & p); //普通函数 声明
+    /*{
         cout << "姓名：" << p.m_Name << "  年龄： " << p.m_Age << endl;
-    }
+    }*/
 public:
     Person(T1 name, T2 age)
     {
@@ -21,6 +27,13 @@ private:
     T1 m_Name;
     T2 m_Age;
 };
+
+//类外实现
+template<class T1 ,class T2>
+void printPerson(Person<T1, T2> & p)
+{
+    cout << "姓名：" << p.m_Name << "  年龄： " << p.m_Age << endl;
+}
 
 void test01()
 {
