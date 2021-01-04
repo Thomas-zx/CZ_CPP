c4cc6cce16c8128862229915ba21c6bfdbd00648
张兴 <zhangxingmsn@gmail.com>
2021-01-04 15:59:21 +0800

    68.类模板碰到继承的问题

--- main.cpp
+++ main.cpp
@@ -1,75 +1,41 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
-//类模板
-template <class NameType, class AgeType = int> //类模板可以有默认类型
-class Person
+template <class T>
+class Base
 {
 public:
-    Person(NameType name, AgeType age)
-    {
-        this->m_Name = name;
-        this->m_Age = age;
-    }
-
-    void showPerson()
-    {
-        cout << "姓名：" << this->m_Name << " 年龄： " << this->m_Age << endl;
-    }
-
-    NameType m_Name;
-    AgeType m_Age;
+    T m_A; //double类型
 };
 
-//1.指定传入类型
-void doWork( Person<string ,int> & p )
+//child继承与 base必须告诉base中的T的类型，否则T无法分配内存
+class Child : public Base<int>
 {
-    p.showPerson();
-}
+
+};
+
+//child2 也是模板类
+template<class T1, class T2>
+class Child2 :public Base<T2>
+{
+public:
+    Child2()
+    {
+        cout << typeid(T1).name() << endl;
+        cout << typeid(T2).name() << endl;
+    }
+public:
+    T1 m_B; //int类型
+};
 
 void test01()
 {
-    Person <string, int> p("MT",10);
-    doWork(p);
-}
-
-//2.参数模板化
-template<class T1 ,class T2>
-void doWork2(Person<T1, T2> & p)
-{
-    //如何查看类型
-    cout << typeid(T1).name() << endl;
-    cout << typeid(T2).name() << endl;
-    p.showPerson();
-}
-void test02()
-{
-    Person <string, int> p("呆贼", 18);
-
-    doWork2(p);
-}
-
-//3.整体模板化
-template<class T>
-void doWork3(T& p)
-{
-    cout << typeid(T).name() << endl;
-    p.showPerson();
-}
-
-void test03()
-{
-    Person <string, int> p("劣人", 18);
-
-    doWork3(p);
+    Child2<int, double> child;//由用户指定类型
 }
 
 int main()
 {
-	//test01();
-    //test02();
-    test03();
+    test01();
 
     system("pause");
     return EXIT_SUCCESS;
