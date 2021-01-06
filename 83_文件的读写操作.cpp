326728fb5cf8c03b9d6f8536c2e0280267495b6f
张兴 <zhangxingmsn@gmail.com>
2021-01-06 14:44:45 +0800

    84.STL三大组件

---
+++ 83_文件的读写操作.cpp
@@ -0,0 +1,77 @@
+//
+// Created by 张兴 on 2021-01-06.
+//
+
+#include <iostream>
+//文件读写头文件
+#include <fstream>
+using namespace std;
+
+//写文件
+void test01()
+{
+    //以输出的方式打开文件
+    //ofstream ofs("./83_test.txt", ios::out | ios::trunc);
+
+    //后期指定打开方式
+    ofstream ofs;
+    ofs.open("./83_test.txt", ios::out | ios::trunc);
+
+    //判断是否打开成功
+    if ( !ofs.is_open() )
+    {
+        cout << "打开失败" << endl;
+    }
+
+    ofs << "姓名：abc" << endl;
+    ofs << "年龄：100" << endl;
+    ofs << "性别：男" << endl;
+
+    ofs.close();
+}
+
+//读文件
+void test02()
+{
+    ifstream ifs;
+    ifs.open("./83_test.txt", ios::in);
+
+    //判断是否打开成功
+    if (!ifs.is_open() )
+    {
+        cout << "打开失败" << endl;
+    }
+
+    //第一种方式
+    char buf[1024];
+    while (ifs >>buf) //按行读取
+    {
+        cout << buf << endl;
+    }
+
+    //第二种方式
+    //char buf2[1024];
+    //while (!ifs.eof()) //eof读到文件尾
+    //{
+    //	ifs.getline(buf2, sizeof(buf2));
+    //	cout << buf2 << endl;
+    //}
+
+    //第三种 不推荐 按单个字符读取
+    //char c;
+    //while ((c = ifs.get() ) != EOF) // EOF文件尾
+    //{
+    //    cout << c;
+    //}
+
+    ifs.close();
+}
+
+int main()
+{
+    //test01();
+    test02();
+
+    system("pause");
+    return EXIT_SUCCESS;
+}
--- main.cpp
+++ main.cpp
@@ -1,72 +1,145 @@
 #include <iostream>
-//文件读写头文件
-#include <fstream>
+#include <string>
+//容器 vector
+#include <vector>
+//使用系统算法的头文件
+#include <algorithm>
+
 using namespace std;
 
-//写文件
 void test01()
 {
-    //以输出的方式打开文件
-    //ofstream ofs("./83_test.txt", ios::out | ios::trunc);
+    int array[5] = {1, 3, 4, 5, 7};
+    int *p = array;  //指针指向数组首地址 &array[0]
 
-    //后期指定打开方式
-    ofstream ofs;
-    ofs.open("./83_test.txt", ios::out | ios::trunc);
-
-    //判断是否打开成功
-    if ( !ofs.is_open())
-    {
-        cout << "打开失败" << endl;
+    for (int i = 0; i < sizeof(array) / sizeof(int); i++) {
+        //cout << array[i] << endl;
+        cout << *(p++) << endl;
     }
-
-    ofs << "姓名：abc" << endl;
-    ofs << "年龄：100" << endl;
-    ofs << "性别：男" << endl;
-
-    ofs.close();
 }
 
-//读文件
+void myPrint(int v)
+{
+    cout << v << endl;
+}
+
 void test02()
 {
-    ifstream ifs;
-    ifs.open("./83_test.txt", ios::in);
+    //声明一个容器 这个容器中存放int类型数据 对象名称array
+    vector<int> array;
 
-    //判断是否打开成功
-    if (!ifs.is_open())
-    {
-        cout << "打开失败" << endl;
-    }
+    //向容器中加入数据
+    array.push_back(10);
+    array.push_back(20);
+    array.push_back(30);
+    array.push_back(40);
 
-    //第一种方式
-    char buf[1024];
-    while (ifs >>buf) //按行读取
-    {
-    	cout << buf << endl;
-    }
+    //遍历容器中的数据
+    //利用迭代器
+    vector<int>::iterator itBegin = array.begin();  // itBegin指向的是v容器中的起始位置
+    vector<int>::iterator itEnd = array.end();  //itEnd指向v容器中最后一个位置的下一个地址
 
-    //第二种方式
-    //char buf2[1024];
-    //while (!ifs.eof()) //eof读到文件尾
-    //{
-    //	ifs.getline(buf2, sizeof(buf2));
-    //	cout << buf2 << endl;
+    //while (itBegin != itEnd) {
+    //    cout << *itBegin << endl;
+    //    itBegin++;
     //}
 
-    //第三种 不推荐 按单个字符读取
-    //char c;
-    //while (  (c = ifs.get() ) != EOF) // EOF文件尾
-    //{
-    //    cout << c;
+    //第二种遍历方式
+    //for (vector<int>::iterator it = array.begin(); it != array.end(); it++) {
+    //    cout << *it << endl;
     //}
 
-    ifs.close();
+    //第三种方式 利用算法
+    for_each(array.begin(), array.end(), myPrint);
 }
 
-int main()
+class Person
+{
+public:
+    Person(string name, int age)
+    {
+        this->mName = name;
+        this->mAge = age;
+    }
+
+    string mName;
+    int mAge;
+};
+
+void test03()
+{
+    vector<Person> v;
+    Person p1("大头儿子", 10);
+    Person p2("小头爸爸", 32);
+    Person p3("隔壁王叔叔", 30);
+    Person p4("围裙妈妈", 28);
+
+    v.push_back(p1);
+    v.push_back(p2);
+    v.push_back(p3);
+    v.push_back(p4);
+
+    //遍历
+    for(vector<Person>::iterator it = v.begin(); it != v.end(); it++) {
+        cout << "姓名: " << (*it).mName << " 年龄：" << it->mAge << endl;
+    }
+}
+
+//存放自定义数据类型的指针
+void test04()
+{
+    vector<Person *> v;
+
+    Person p1("大头儿子", 10);
+    Person p2("小头爸爸", 32);
+    Person p3("隔壁王叔叔", 30);
+    Person p4("围裙妈妈", 28);
+
+    v.push_back(&p1);
+    v.push_back(&p2);
+    v.push_back(&p3);
+    v.push_back(&p4);
+
+    for(vector<Person *>::iterator it = v.begin(); it != v.end(); it++) {
+        cout << "姓名：： " << (*it)->mName << " 年龄： " << (*it)->mAge << endl;
+    }
+}
+
+//容器嵌套容器
+void test05()
+{
+    vector<vector<int>> v;
+
+    vector<int>v1;
+    vector<int>v2;
+    vector<int>v3;
+
+    for (int i = 0; i < 5; i++)
+    {
+        v1.push_back(i);
+        v2.push_back(i + 10);
+        v3.push_back(i + 100);
+    }
+
+    v.push_back(v1);
+    v.push_back(v2);
+    v.push_back(v3);
+
+    for (vector<vector<int>>::iterator it = v.begin(); it != v.end(); it++) {
+        for (vector<int>::iterator vit = (*it).begin(); vit != (*it).end(); vit++) {
+            cout << *vit << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main(int argc, char *argv[])
 {
     //test01();
-    test02();
+    //test02();
+    //test03();
+    //test04();
+    test05();
 
     system("pause");
     return EXIT_SUCCESS;
