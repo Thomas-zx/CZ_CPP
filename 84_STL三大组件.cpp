38b1f7eb3b02236b2cb4666ec8f84d1df765f9a5
张兴 <zhangxingmsn@gmail.com>
2021-01-06 15:12:08 +0800

    85.string容器

---
+++ 84_STL三大组件.cpp
@@ -0,0 +1,149 @@
+//
+// Created by 张兴 on 2021-01-06.
+//
+
+#include <iostream>
+//容器 vector
+#include <vector>
+//使用系统算法的头文件
+#include <algorithm>
+
+using namespace std;
+
+void test01()
+{
+    int array[5] = {1, 3, 4, 5, 7};
+    int *p = array;  //指针指向数组首地址 &array[0]
+
+    for (int i = 0; i < sizeof(array) / sizeof(int); i++) {
+        //cout << array[i] << endl;
+        cout << *(p++) << endl;
+    }
+}
+
+void myPrint(int v)
+{
+    cout << v << endl;
+}
+
+void test02()
+{
+    //声明一个容器 这个容器中存放int类型数据 对象名称array
+    vector<int> array;
+
+    //向容器中加入数据
+    array.push_back(10);
+    array.push_back(20);
+    array.push_back(30);
+    array.push_back(40);
+
+    //遍历容器中的数据
+    //利用迭代器
+    vector<int>::iterator itBegin = array.begin();  // itBegin指向的是v容器中的起始位置
+    vector<int>::iterator itEnd = array.end();  //itEnd指向v容器中最后一个位置的下一个地址
+
+    //while (itBegin != itEnd) {
+    //    cout << *itBegin << endl;
+    //    itBegin++;
+    //}
+
+    //第二种遍历方式
+    //for (vector<int>::iterator it = array.begin(); it != array.end(); it++) {
+    //    cout << *it << endl;
+    //}
+
+    //第三种方式 利用算法
+    for_each(array.begin(), array.end(), myPrint);
+}
+
+class Person
+{
+public:
+    Person(string name, int age)
+    {
+        this->mName = name;
+        this->mAge = age;
+    }
+
+    string mName;
+    int mAge;
+};
+
+void test03()
+{
+    vector<Person> v;
+    Person p1("大头儿子", 10);
+    Person p2("小头爸爸", 32);
+    Person p3("隔壁王叔叔", 30);
+    Person p4("围裙妈妈", 28);
+
+    v.push_back(p1);
+    v.push_back(p2);
+    v.push_back(p3);
+    v.push_back(p4);
+
+    //遍历
+    for(vector<Person>::iterator it = v.begin(); it != v.end(); it++) {
+        cout << "姓名: " << (*it).mName << " 年龄：" << it->mAge << endl;
+    }
+}
+
+//存放自定义数据类型的指针
+void test04()
+{
+    vector<Person *> v;
+
+    Person p1("大头儿子", 10);
+    Person p2("小头爸爸", 32);
+    Person p3("隔壁王叔叔", 30);
+    Person p4("围裙妈妈", 28);
+
+    v.push_back(&p1);
+    v.push_back(&p2);
+    v.push_back(&p3);
+    v.push_back(&p4);
+
+    for(vector<Person *>::iterator it = v.begin(); it != v.end(); it++) {
+        cout << "姓名：： " << (*it)->mName << " 年龄： " << (*it)->mAge << endl;
+    }
+}
+
+//容器嵌套容器
+void test05()
+{
+    vector<vector<int>> v;
+
+    vector<int>v1;
+    vector<int>v2;
+    vector<int>v3;
+
+    for (int i = 0; i < 5; i++)
+    {
+        v1.push_back(i);
+        v2.push_back(i + 10);
+        v3.push_back(i + 100);
+    }
+
+    v.push_back(v1);
+    v.push_back(v2);
+    v.push_back(v3);
+
+    for (vector<vector<int>>::iterator it = v.begin(); it != v.end(); it++) {
+        for (vector<int>::iterator vit = (*it).begin(); vit != (*it).end(); vit++) {
+            cout << *vit << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    //test01();
+    //test02();
+    //test03();
+    //test04();
+    test05();
+
+    system("pause");
+    return EXIT_SUCCESS;
+}
--- main.cpp
+++ main.cpp
@@ -1,136 +1,266 @@
 #include <iostream>
 #include <string>
-//容器 vector
-#include <vector>
-//使用系统算法的头文件
-#include <algorithm>
+#include <stdexcept>
 
 using namespace std;
+/*
+string 构造函数
+string();//创建一个空的字符串 例如: string str;
+string(const string& str);//使用一个string对象初始化另一个string对象
+string(const char* s);//使用字符串s初始化
+string(int n, char c);//使用n个字符c初始化
+
+3.1.2.2 string基本赋值操作
+string& operator=(const char* s);//char*类型字符串 赋值给当前的字符串
+string& operator=(const string &s);//把字符串s赋给当前的字符串
+string& operator=(char c);//字符赋值给当前的字符串
+string& assign(const char *s);//把字符串s赋给当前的字符串
+string& assign(const char *s, int n);//把字符串s的前n个字符赋给当前的字符串
+string& assign(const string &s);//把字符串s赋给当前字符串
+string& assign(int n, char c);//用n个字符c赋给当前字符串
+string& assign(const string &s, int start, int n);//将s从start开始n个字符赋值给字符串
+*/
 
 void test01()
 {
-    int array[5] = {1, 3, 4, 5, 7};
-    int *p = array;  //指针指向数组首地址 &array[0]
+    string str;  //默认构造
+    string str2(str);  //拷贝构造
+    string str3  = str;
+    string str4 = "abcd";
+    string str5(10, 'a');
 
-    for (int i = 0; i < sizeof(array) / sizeof(int); i++) {
-        //cout << array[i] << endl;
-        cout << *(p++) << endl;
-    }
+    cout << str4 << endl;
+    cout << str5 << endl;
+
+    //基本赋值
+    str = "hello";
+    str2 = str4;
+
+    //string& assign(const char *s, int n);//把字符串s的前n个字符赋给当前的字符串
+    str3.assign("abcdef", 4);
+    cout << str3 << endl;
+
+    //string& assign(const string &s, int start, int n);//将s从start开始n个字符赋值给字符串
+    string str6;
+    str6.assign(str, 1, 3);  //ell ? hel 从0索引
+    cout << str6 << endl;
 }
 
-void myPrint(int v)
-{
-    cout << v << endl;
-}
-
+/*
+string存取字符操作
+char& operator[](int n);//通过[]方式取字符
+char& at(int n);//通过at方法获取字符
+*/
 void test02()
 {
-    //声明一个容器 这个容器中存放int类型数据 对象名称array
-    vector<int> array;
+    string s = "hello world";
 
-    //向容器中加入数据
-    array.push_back(10);
-    array.push_back(20);
-    array.push_back(30);
-    array.push_back(40);
-
-    //遍历容器中的数据
-    //利用迭代器
-    vector<int>::iterator itBegin = array.begin();  // itBegin指向的是v容器中的起始位置
-    vector<int>::iterator itEnd = array.end();  //itEnd指向v容器中最后一个位置的下一个地址
-
-    //while (itBegin != itEnd) {
-    //    cout << *itBegin << endl;
-    //    itBegin++;
-    //}
-
-    //第二种遍历方式
-    //for (vector<int>::iterator it = array.begin(); it != array.end(); it++) {
-    //    cout << *it << endl;
-    //}
-
-    //第三种方式 利用算法
-    for_each(array.begin(), array.end(), myPrint);
-}
-
-class Person
-{
-public:
-    Person(string name, int age)
-    {
-        this->mName = name;
-        this->mAge = age;
+    for (int i = 0; i < s.size(); i++) {
+        //cout << s[i] << endl;
+        //[] 和at区别？[]访问越界 直接挂掉 at会抛出异常
+        //cout << s.at(i) << endl;
     }
 
-    string mName;
-    int mAge;
-};
+    try
+    {
+        //cout << s[100] << endl;
+        cout << s.at(1000) << endl;
+    }
+    catch (out_of_range & e)
+    {
+        cout << e.what() << endl;
+    }
+    catch (...)
+    {
+        cout << "越界异常" << endl;
+    }
+}
+
+/*
+string拼接操作
+string& operator+=(const string& str);//重载+=操作符
+string& operator+=(const char* str);//重载+=操作符
+string& operator+=(const char c);//重载+=操作符
+string& append(const char *s);//把字符串s连接到当前字符串结尾
+string& append(const char *s, int n);//把字符串s的前n个字符连接到当前字符串结尾
+string& append(const string &s);//同operator+=()
+string& append(const string &s, int pos, int n);//把字符串s中从pos开始的n个字符连接到当前字符串结尾
+string& append(int n, char c);//在当前字符串结尾添加n个字符c
+
+3.1.2.5 string查找和替换
+int find(const string& str, int pos = 0) const; //查找str第一次出现位置,从pos开始查找
+int find(const char* s, int pos = 0) const;  //查找s第一次出现位置,从pos开始查找
+int find(const char* s, int pos, int n) const;  //从pos位置查找s的前n个字符第一次位置
+int find(const char c, int pos = 0) const;  //查找字符c第一次出现位置
+int rfind(const string& str, int pos = npos) const;//查找str最后一次位置,从pos开始查找
+int rfind(const char* s, int pos = npos) const;//查找s最后一次出现位置,从pos开始查找
+int rfind(const char* s, int pos, int n) const;//从pos查找s的前n个字符最后一次位置
+int rfind(const char c, int pos = 0) const; //查找字符c最后一次出现位置
+string& replace(int pos, int n, const string& str); //替换从pos开始n个字符为字符串str
+string& replace(int pos, int n, const char* s); //替换从pos开始的n个字符为字符串s
+*/
 
 void test03()
 {
-    vector<Person> v;
-    Person p1("大头儿子", 10);
-    Person p2("小头爸爸", 32);
-    Person p3("隔壁王叔叔", 30);
-    Person p4("围裙妈妈", 28);
+    //拼接
+    string s1 = "我";
+    string s2 = "爱北京";
 
-    v.push_back(p1);
-    v.push_back(p2);
-    v.push_back(p3);
-    v.push_back(p4);
+    s1 += s2;
+    cout << s1 << endl;
 
-    //遍历
-    for(vector<Person>::iterator it = v.begin(); it != v.end(); it++) {
-        cout << "姓名: " << (*it).mName << " 年龄：" << it->mAge << endl;
-    }
+    s1.append("天安门");
+    cout << s1 << endl;
+
+    //find查找
+    string s = "abcdefg";
+    int pos = s.find("bcd");  //找不到返回是 -1
+    cout << "pos = " << pos << endl;
+
+    int pos2 = s.rfind("bc"); //rfind  和find 结果一样，内部查找顺序相反
+    cout << "pos2 = " << pos2 << endl; // 4 2
+
+    //替换
+    string s3 = "hello"; //替换从pos开始n个字符为字符串str
+    s3.replace(1, 3, "1111");
+    cout << s3 << endl; // h1111o
 }
 
-//存放自定义数据类型的指针
+/*
+string比较操作
+
+compare函数在>时返回 1，<时返回 -1，==时返回 0。
+比较区分大小写，比较时参考字典顺序，排越前面的越小。
+大写的A比小写的a小。
+
+int compare(const string &s) const;//与字符串s比较
+int compare(const char *s) const;//与字符串s比较
+*/
+
 void test04()
 {
-    vector<Person *> v;
+    string s1 = "abc";
+    string s2 = "abcd";
 
-    Person p1("大头儿子", 10);
-    Person p2("小头爸爸", 32);
-    Person p3("隔壁王叔叔", 30);
-    Person p4("围裙妈妈", 28);
-
-    v.push_back(&p1);
-    v.push_back(&p2);
-    v.push_back(&p3);
-    v.push_back(&p4);
-
-    for(vector<Person *>::iterator it = v.begin(); it != v.end(); it++) {
-        cout << "姓名：： " << (*it)->mName << " 年龄： " << (*it)->mAge << endl;
+    if (s1.compare(s2) == 0)
+    {
+        cout << "s1 等于 s2" << endl;
+    }
+    else if (s1.compare(s2) == 1)
+    {
+        cout << "s1 大于 s2" << endl;
+    }
+    else
+    {
+        cout << "s1 小于 s2" << endl;
     }
 }
 
-//容器嵌套容器
+/*
+string子串
+string substr(int pos = 0, int n = npos) const;//返回由pos开始的n个字符组成的字符串
+*/
+
 void test05()
 {
-    vector<vector<int>> v;
+    string s1 = "abcde";
 
-    vector<int>v1;
-    vector<int>v2;
-    vector<int>v3;
+    string s2 = s1.substr(1, 3);
+    cout << "s2 = " << s2 << endl;
 
-    for (int i = 0; i < 5; i++)
+    //需求  查找一个右键的 用户名
+    string email = "zhangtao@sina.com";
+
+    int pos = email.find("@");  //8
+    cout << "pos " << pos << endl;
+
+    string usrName = email.substr(0, pos);
+    cout << "用户名为：" << usrName << endl;
+}
+
+/*
+string插入和删除操作
+string& insert(int pos, const char* s); //插入字符串
+string& insert(int pos, const string& str); //插入字符串
+string& insert(int pos, int n, char c);//在指定位置插入n个字符c
+string& erase(int pos, int n = npos);//删除从Pos开始的n个字符
+*/
+
+void test06()
+{
+    string s1 = "hello";
+    s1.insert(1, "111");
+    cout << s1 << endl; //h111ello
+
+    //删除 111
+    s1.erase(1, 3);
+    cout << s1 << endl;
+}
+
+/*
+string和c-style字符串转换
+*/
+void func(string s)
+{
+    cout << s << endl;
+}
+
+void func2(const char * s)
+{
+    cout << s << endl;
+}
+
+void test07()
+{
+    string s = "abc";
+    //string -> const char *
+
+    const char * p = s.c_str();
+
+    func(p);  //const char * 隐式类型转换为 string
+
+    //const char * -> string
+
+    string s2(p);
+    //func2(s2);  //string 不能隐式类型转换为 char *
+}
+
+void test08()
+{
+    string s = "abcdefg";
+    char& a = s[2];
+    char& b = s[3];
+
+    a = '1';
+    b = '2';
+
+    cout << s << endl;
+    cout << (int*)s.c_str() << endl;
+
+    s = "pppppppppppppp";
+
+    a = '1';
+    b = '2';
+
+    cout << s << endl;
+    cout << (int*)s.c_str() << endl;
+}
+
+/*
+写一个函数，函数内部将string字符串中的所有小写字母都变为大写字母。
+*/
+void test09()
+{
+    string s = "abCdEfg";
+
+    for (int i = 0; i < s.size(); i++)
     {
-        v1.push_back(i);
-        v2.push_back(i + 10);
-        v3.push_back(i + 100);
+        //s[i] = toupper(s[i]);
+        //全变小写
+        s[i] = tolower(s[i]);
     }
 
-    v.push_back(v1);
-    v.push_back(v2);
-    v.push_back(v3);
-
-    for (vector<vector<int>>::iterator it = v.begin(); it != v.end(); it++) {
-        for (vector<int>::iterator vit = (*it).begin(); vit != (*it).end(); vit++) {
-            cout << *vit << " ";
-        }
-        cout << endl;
-    }
+    cout << s << endl;
 }
 
 int main(int argc, char *argv[])
@@ -139,7 +269,11 @@ int main(int argc, char *argv[])
     //test02();
     //test03();
     //test04();
-    test05();
+    //test05();
+    //test06();
+    //test07();
+    //test08();
+    test09();
 
     system("pause");
     return EXIT_SUCCESS;
